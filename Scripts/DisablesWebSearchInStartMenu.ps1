1502d0b21646e8a9e15bb78d6333ecb102bfb106
TheKitoInc <11139894+TheKitoInc@users.noreply.github.com>
2024-08-05 09:23:58 -0300

    DisablesWebSearchInStartMenu

---
+++ Scripts/DisablesWebSearchInStartMenu.ps1
@@ -0,0 +1,12 @@
+    #Disables Web Search in Start Menu
+    Write-Output "Disabling Bing Search in Start Menu"
+
+    $WebSearch = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search"
+
+    Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" BingSearchEnabled -Value 0 
+
+    If (!(Test-Path $WebSearch)) {
+        New-Item $WebSearch
+    }
+
+    Set-ItemProperty $WebSearch DisableWebSearch -Value 1 
