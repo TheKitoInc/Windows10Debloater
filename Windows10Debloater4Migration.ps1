87d93a5e40b09cd355b87404be5b480d607fb723
TheKitoInc <11139894+TheKitoInc@users.noreply.github.com>
2024-08-05 09:25:41 -0300

    DisablesWebSearchInStartMenu

--- Windows10Debloater4Migration.ps1
+++ Windows10Debloater4Migration.ps1
@@ -186,14 +186,7 @@ Function Protect-Privacy {
         Set-ItemProperty $Search AllowCortana -Value 0 
     }
 
-    #Disables Web Search in Start Menu
-    Write-Output "Disabling Bing Search in Start Menu"
-    $WebSearch = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search"
-    Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" BingSearchEnabled -Value 0 
-    If (!(Test-Path $WebSearch)) {
-        New-Item $WebSearch
-    }
-    Set-ItemProperty $WebSearch DisableWebSearch -Value 1 
+
             
     #Stops the Windows Feedback Experience from sending anonymous data
     Write-Output "Stopping the Windows Feedback Experience program"
